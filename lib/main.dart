ca13b7f1483d159d133d22c85eeee9690e2b748e
HadiMo313 <hadimustafa26@gmail.com>
2023-05-05 11:00:39 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'bateekh',
+      title: 'melbin',
+      color: Colors.red,
       theme: ThemeData(
         // This is the theme of your application.
         //
